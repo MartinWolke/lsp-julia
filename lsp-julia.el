0f026de003dbb166facf5484495ae1c647bd0315
MartinWolke <martin.woelck@gmail.com>
2017-08-17 17:05:54 +0200

    initial commit

---
+++ lsp-julia.el
@@ -0,0 +1,19 @@
+(require 'ess-site)
+(require 'lsp-mode)
+
+(defun lsp-julia--get-root ()
+  "Try to find the package directory by searching for a .gitignore file.
+If no .gitignore file can be found use the default directory "
+  (or (expand-file-name (locate-dominating-file default-directory ".gitignore"))
+      default-directory))
+
+
+(defun lsp-julia--rls-command ()
+  `("julia" "--startup-file=no" "--history-file=no" "-e"
+    "using LanguageServer; server = LanguageServer.LanguageServerInstance(STDIN, STDOUT, false); server.runlinter = true; run(server);"))
+
+(lsp-define-stdio-client 'ess-julia-mode "julia" 'stdio #'lsp-julia--get-root
+                         "Julia Language Server"
+                         (lsp-julia--rls-command))
+
+(provide 'lsp-julia)
