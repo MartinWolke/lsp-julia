72aa2f6b5f243fbabafcb3841e6614378df87c71
Martin Woelck <martin.woelck@gmail.com>
2019-10-06 09:36:15 +0200

    update to new lsp version

--- lsp-julia.el
+++ lsp-julia.el
@@ -1,19 +1,50 @@
-(require 'ess-site)
-(require 'julia-mode)
 (require 'lsp-mode)
 
+(defcustom lsp-julia-default-environment "~/.julia/environments/v1.1"
+  "The path to the default environment."
+  :type 'string
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-command "/home/martin/git/julia-versions/julia-12/julia"
+  "Command to invoke julia with."
+  :type 'string
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-flags '("--startup-file=no" "--history-file=no")
+  "List of additional flags to call julia with."
+  :type '(repeat (string :tag "argument"))
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-timeout 30
+  "Time before lsp-mode should assume julia just ain't gonna start."
+  :type 'number
+  :group 'lsp-julia)
+
+(defcustom lsp-julia-default-depot ""
+  "The default depot path, used if `JULIA_DEPOT_PATH' is unset"
+  :type 'string
+  :group 'lsp-julia)
+
 (defun lsp-julia--get-root ()
-  "Try to find the package directory by searching for a .gitignore file.
-If no .gitignore file can be found use the default directory "
-  (let ((dir (locate-dominating-file default-directory ".gitignore")))
-    (if dir
-        (expand-file-name dir)
-      default-directory)))
+  (let ((dir (locate-dominating-file default-directory "Project.toml")))
+    (if dir (expand-file-name dir)
+      (expand-file-name lsp-julia-default-environment))))
+
+(defun lsp-julia--get-depot-path ()
+  (let ((dp (getenv "JULIA_DEPOT_PATH")))
+    (if dp dp lsp-julia-default-depot)))
 
 (defun lsp-julia--rls-command ()
-  `("julia" "--startup-file=no" "--history-file=no" "-e"
-    "using LanguageServer; server = LanguageServer.LanguageServerInstance(STDIN, STDOUT, false); server.runlinter = true; run(server);"))
-
+  `(,lsp-julia-command
+    ,@lsp-julia-flags
+    ,(concat "-e using LanguageServer, Sockets, SymbolServer;"
+             " server = LanguageServer.LanguageServerInstance("
+             " stdin, stdout, false,"
+             " \"" (lsp-julia--get-root) "\","
+             " \"" (lsp-julia--get-depot-path) "\","
+             "Dict());"
+             ;; " server.runlinter = true;"
+             " run(server);")))
 
 (defconst lsp-julia--handlers
   '(("window/setStatusBusy" .
@@ -21,11 +52,9 @@ If no .gitignore file can be found use the default directory "
     ("window/setStatusReady" .
      (lambda(w _p)))))
 
-(defun lsp-julia--initialize-client(client)
-  (mapcar #'(lambda (p) (lsp-client-on-notification client (car p) (cdr p))) lsp-julia--handlers))
+(lsp-register-client
+ (make-lsp-client :new-connection (lsp-stdio-connection 'lsp-julia--rls-command)
+                  :major-modes '(julia-mode)
+                  :server-id 'julia-ls))
 
-(lsp-define-stdio-client lsp-julia "julia" #'lsp-julia--get-root nil
-                         :command-fn #'lsp-julia--rls-command
-                         :initialize #'lsp-julia--initialize-client)
-
-(provide 'lsp-julia)
+(provide 'lsp-julia) 
